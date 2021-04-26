6dd0df99050a2603c623602f65ebc79908951e6c
katiegwilliams <kwilliams03@utexas.edu>
2021-04-26 11:11:05 -0500

    separate analyses file for BUCLD abstract (English-speakers only)

--- BUCLD_Analyses.R
+++ BUCLD_Analyses.R
@@ -13,6 +13,7 @@ library(coin)
 library(scales)  # for scale_y_continuous(label = percent)
 library(psych) # for calculating ICC reliability check on timeline task
 library(ggthemes) # for scale_fill_few('medium')
+library(broom) # tukeyHSD posthoc test
 
 # read in the data
 d.e <- read.csv('data_austin.csv')
@@ -153,8 +154,8 @@ d.e <- d.e %>%
                                 task == "timeline" & linenum == "4" & item == "beforeyesterday" & between(distfrommid, -7, -3.5) ~ '1',
                                 task == "timeline" & linenum == "4" & item == "yesterday" & between(distfrommid, -3.5, 0) ~ '1',
                                 task == "timeline" & linenum == "4" & item == "tomorrow" & between(distfrommid, 0, 3.5) ~ '1',
-                                task == "timeline" & linenum == "4" & item == "aftertomorrow" & between(distfrommid, 3.5, 7) ~ '1'),
-         cor.remote = case_when(cor.remote == NA ~ '0'),
+                                task == "timeline" & linenum == "4" & item == "aftertomorrow" & between(distfrommid, 3.5, 7) ~ '1',
+                                TRUE ~ '0'),
          cor.remote = as.numeric(cor.remote))
 
 # participant counts
@@ -166,7 +167,269 @@ subs_counts <- subs %>%
   group_by(agegroup) %>%
   summarize(n())
 
-#Does age or item predict successful placement of time words in the past vs. the future within each language group separately? [no interaction term]
-d.lm.2e <-  glmer(stat.correct.1 ~ prox + scale(ageyears) + (1|subjid), family='binomial', data=cal.e) 
-summary(d.lm.2e)
-Anova(d.lm.2e, Type=3) #main effect of age
+#### Analysis of past/future assignment on the calendar task ####
+
+# create data frame with only deictic calendar items
+cal.d <- d.e %>% 
+  filter(task=="calendar" & itemtype=="deictic")
+cal.d2 <- cal.d %>%
+  select(-linelength,-distfrommid) %>%
+  mutate(cor.first = ifelse(response1==correctR, 1, 0), # did they get it right on the first trial
+         prox = ifelse(item %in% c('yesterday','tomorrow'),1,0), # yesterday and tomorrow coded as proximal terms
+         prox = as.factor(prox))
+
+# summarize data including 3- and 7-year-olds
+cal.sum <- cal.d2 %>% 
+  mutate(item = factor(item, levels = c("beforeyesterday", "aftertomorrow","yesterday","tomorrow")),
+         agegroup = recode_factor(agegroup, '3' = 'Age 3',
+                                '4' = 'Age 4',
+                                '5' = 'Age 5',
+                                '6' = 'Age 6',
+                                '7' = 'Age 7')) %>%
+  group_by(agegroup, item) %>%
+  summarize(first.m = mean(cor.first, na.rm=T), # % who put item in right box on first try
+            sd.correct = sd(cor.first, na.rm=T),
+            n = n(),
+            se.correct = sd.correct/sqrt(n),
+            deictic1.m = mean(stat.correct.1), # who got correct status (first answer)
+            deictic.m = mean(stat.correct), # % who got correct status (final answer)
+            deictic1.sd = (sd(stat.correct.1)), # standard deviation
+            deictic1.se = deictic1.sd/sqrt(n), # calculate standard error
+            deictic1.lower = deictic1.m - deictic1.se, # calculate lower 95% CI
+            deictic1.upper = deictic1.m + deictic1.se, # calculate upper 95% CI
+            rank.m = mean(correct), # right box (final answer)
+            countCor.first = sum(cor.first))  # sum proportion correct for making bar graph instead of histogram
+
+#Does age or item (prox vs. distal status) predict successful placement of time words in the past vs. the future? [interaction term included]
+d.lm.ec1 <-  glmer(stat.correct.1 ~ prox*scale(ageyears) + (1|subjid), family='binomial', data=cal.d2) 
+summary(d.lm.ec1)
+Anova(d.lm.ec1, Type=3) 
+
+#now check for main effects of age and item [no interaction term included in the model]
+d.lm.ec2 <-  glmer(stat.correct.1 ~ prox+scale(ageyears) + (1|subjid), family='binomial', data=cal.d2) 
+summary(d.lm.ec2)
+Anova(d.lm.ec2, Type=3) #main effect of age
+
+Temp.PF2 <- aggregate(stat.correct.1 ~ agegroup + ageyears + subjid, subset(cal.d2), mean)
+with(subset(Temp.PF2, agegroup=="3"), t.test(x=stat.correct.1, mu=.5))
+with(subset(Temp.PF2, agegroup=="4"), t.test(x=stat.correct.1, mu=.5))
+with(subset(Temp.PF2, agegroup=="5"), t.test(x=stat.correct.1, mu=.5))
+with(subset(Temp.PF2, agegroup=="6"), t.test(x=stat.correct.1, mu=.5))
+with(subset(Temp.PF2, agegroup=="7"), t.test(x=stat.correct.1, mu=.5))
+with(subset(Temp.PF2, agegroup %in% c("4", "5")), t.test(stat.correct.1 ~ agegroup, var.equal=T))
+
+# Plot correct deictic status on the calendar task
+correctStatus_cal <- ggplot(data=cal.sum, 
+                        aes(x=agegroup, y=deictic1.m, group = item, color=item)) +
+  #geom_line(aes(linetype=item)) +
+  geom_line(aes(color=item))+
+  #scale_linetype_manual(values=c("twodash", "dotted", "solid", "longdash"))+
+  geom_point() +
+  ylab('Proportion Correct') +
+  xlab('') +
+  ylim(0,1) +
+  theme_minimal(base_size = 14) +
+  theme(plot.title=element_text(family = "Times", color = "black", size = 14, hjust = 0.5), 
+        axis.title.y = element_text(family = "Times", color = "black", size = 14),
+        axis.line.y = element_line(color = "black"),
+        axis.text.y = element_text(family = "Times", color = "black", size = 14), 
+        axis.title.x = element_text(family = "Times", color = "black", size = 14),
+        axis.text.x = element_text(family = "Times", color = "black", size = 14)) +
+  theme(legend.position = c(0.7, 0.2)) +
+  theme(panel.border = element_rect(colour = "black", fill = NA, size = 1),
+        #panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        axis.line = element_line(colour = "black"))
+correctStatus_cal
+
+#### Analysis of past/future assignment on the timeline task ####
+d.e.T <- d.e %>%
+  subset(task == "timeline" & itemtype == "deictic" & linenum == "4") # restricting analyses to timeline 4 so items are matched across the timeline and calendar task
+
+# Past/Future -- do subjects attribute P items to left of now and F items to right of now?
+d.e.T2 <- aggregate(stat.correct ~ agegroup + subjid + ageyears + item, d.e.T, mean)
+d.e.T2$item <- factor(d.e.T2$item) # filtered out event words so adding item to the model rather than itemtype
+d.e.T2$tBNRight <- asin(sqrt(d.e.T2$stat.correct))
+
+#Does age or item predict successful placement of time words in the past vs. the future? [interaction term included]
+d.lm.eT <-  glmer(stat.correct ~ item*scale(ageyears) + (1|subjid), family='binomial', data=d.e.T2) 
+summary(d.lm.eT)
+Anova(d.lm.eT, Type=3) 
+
+#now check for main effects of age and item [no interaction term included in the model]
+d.lm.eT <-  glmer(stat.correct ~ item + scale(ageyears) + (1|subjid), family='binomial', data=d.e.T2) 
+summary(d.lm.eT)
+Anova(d.lm.eT, Type=3) 
+
+# analysis of variance model
+stat.correct.aov <- aov(stat.correct ~ ageyears * item + Error(subjid/item),d.e.T2)
+summary(stat.correct.aov) #Effects of Age
+# pairwise tests to check item level effects
+pairwise.t.test(d.e.T2$stat.correct, d.e.T2$item, p.adj = "bonf")
+
+
+Temp.PF3 <- aggregate(stat.correct ~ agegroup + ageyears + subjid, subset(d.e.T2), mean)
+with(subset(Temp.PF3, agegroup=="3"), t.test(x=stat.correct, mu=.5))
+with(subset(Temp.PF3, agegroup=="4"), t.test(x=stat.correct, mu=.5))
+with(subset(Temp.PF3, agegroup=="5"), t.test(x=stat.correct, mu=.5))
+with(subset(Temp.PF3, agegroup=="6"), t.test(x=stat.correct, mu=.5))
+with(subset(Temp.PF3, agegroup=="7"), t.test(x=stat.correct, mu=.5))
+with(subset(Temp.PF3, agegroup %in% c("4", "5")), t.test(stat.correct ~ agegroup, var.equal=T))
+
+
+# summarize timeline deictic status knowledge
+timeline.sum <- d.e.T %>% 
+  mutate(item = factor(item, levels = c("beforeyesterday", "aftertomorrow","yesterday","tomorrow")),
+         agegroup = recode_factor(agegroup, '3' = 'Age 3',
+                                  '4' = 'Age 4',
+                                  '5' = 'Age 5',
+                                  '6' = 'Age 6',
+                                  '7' = 'Age 7')) %>%
+  group_by(agegroup, item) %>%
+  summarize(n = n(),
+            deictic1.m = mean(stat.correct.1), # who got correct status (first answer)
+            deictic.m = mean(stat.correct), # % who got correct status (final answer)
+            deictic1.sd = (sd(stat.correct.1)), # standard deviation
+            deictic1.se = deictic1.sd/sqrt(n), # calculate standard error
+            deictic1.lower = deictic1.m - deictic1.se, # calculate lower 95% CI
+            deictic1.upper = deictic1.m + deictic1.se) # calculate upper 95% CI
+
+# Plot correct deictic status on the timeline
+correctStatus_time <- ggplot(data=timeline.sum, 
+                        aes(x=agegroup, y=deictic1.m, group = item, color=item)) +
+  #geom_line(aes(linetype=item)) +
+  geom_line(aes(color=item))+
+  #scale_linetype_manual(values=c("twodash", "dotted", "solid", "longdash"))+
+  geom_point() +
+  ylab('Proportion Correct') +
+  xlab('') +
+  ylim(0,1) +
+  theme_minimal(base_size = 14) +
+  theme(plot.title=element_text(family = "Times", color = "black", size = 14, hjust = 0.5), 
+        axis.title.y = element_text(family = "Times", color = "black", size = 14),
+        axis.line.y = element_line(color = "black"),
+        axis.text.y = element_text(family = "Times", color = "black", size = 14), 
+        axis.title.x = element_text(family = "Times", color = "black", size = 14),
+        axis.text.x = element_text(family = "Times", color = "black", size = 14)) +
+  theme(legend.position = c(0.7, 0.2)) +
+  theme(panel.border = element_rect(colour = "black", fill = NA, size = 1),
+        #panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        axis.line = element_line(colour = "black"))
+correctStatus_time
+
+# combine timeline data and calendar data plots into one figure
+CorrectStatusFigure <- ggarrange(correctStatus_cal, correctStatus_time,
+                                 ncol = 2, nrow = 1, common.legend = TRUE, legend = "top")
+# save figure as a .jpeg file
+ggsave(CorrectStatusFigure, file="correctStatusFigure.jpeg", width = 10, height = 5, dpi = 300)
+
+
+# compare past/future placement on timeline vs calendar task
+d.e.Tc <- rbind(cal.d, d.e.T)
+
+TC_sum <- d.e.Tc %>%
+  group_by(agegroup, task) %>%
+  summarize(n = n(),
+            deictic1.m = mean(stat.correct.1), # who got correct status (first answer)
+            deictic.m = mean(stat.correct), # % who got correct status (final answer)
+            deictic1.sd = (sd(stat.correct.1)), # standard deviation
+            deictic1.se = deictic1.sd/sqrt(n), # calculate standard error
+            deictic1.lower = deictic1.m - deictic1.se, # calculate lower 95% CI
+            deictic1.upper = deictic1.m + deictic1.se) # calculate upper 95% CI
+
+ggplot(TC_sum, aes(task, deictic1.m)) +
+  geom_boxplot()
+t.test(stat.correct.1 ~ task, data = d.e.Tc)
+
+
+
+# calculate percentage of trials in which kids of each age group responded with correct remoteness on calendar questions
+cal.sum2 <- cal.d %>%
+  group_by(agegroup, item) %>%
+  summarize(n = n(),
+            remoteness.m = (mean(cor.remote))*100, # who got correct status (first answer)
+            remoteness.sd = (sd(cor.remote))*100, # standard deviation
+            remoteness.se = remoteness.sd/sqrt(n), # calculate standard error
+            remoteness.lower = remoteness.m - remoteness.se, # calculate lower 95% CI
+            remoteness.upper = remoteness.m + remoteness.se) # calculate upper 95% CI
+
+#Does age or item predict placement of time words the exact distance from the present? [interaction term included]
+d.lm.ec3 <-  glmer(cor.remote ~ prox*scale(ageyears) + (1|subjid), family='binomial', data=cal.d2) 
+summary(d.lm.ec3)
+Anova(d.lm.ec3, Type=3) 
+
+#now check for main effects of age and item [no interaction term included in the model]
+d.lm.ec4 <-  glmer(cor.remote ~ item + scale(ageyears) + (1|subjid), family='binomial', data=cal.d2) 
+summary(d.lm.ec4)
+Anova(d.lm.ec4, Type=3) 
+
+Temp.R3 <- aggregate(cor.remote ~ agegroup + ageyears + subjid, subset(cal.d2), mean)
+with(subset(Temp.R3, agegroup=="3"), t.test(x=cor.remote, mu=.5))
+with(subset(Temp.R3, agegroup=="4"), t.test(x=cor.remote, mu=.5))
+with(subset(Temp.R3, agegroup=="5"), t.test(x=cor.remote, mu=.5))
+with(subset(Temp.R3, agegroup=="6"), t.test(x=cor.remote, mu=.5))
+with(subset(Temp.R3, agegroup=="7"), t.test(x=cor.remote, mu=.5))
+with(subset(Temp.R3, agegroup %in% c("4", "5")), t.test(cor.remote ~ agegroup, var.equal=T))
+
+ggplot(cal.sum2, aes(item, remoteness.m)) +
+  geom_boxplot()
+
+
+# Analyses of ordinal rank
+# create new variable to code rank
+d.e.T <- d.e.T %>%
+mutate(cor.rank = case_when(item == "beforeyesterday" ~ '1',
+                            item == "yesterday"  ~ '2',
+                            item == "tomorrow"  ~ '3',
+                            item == "aftertomorrow" ~ '4',
+                            TRUE ~ '0'),
+       cor.rank = as.numeric(cor.rank),
+       rankCor = ifelse(cor.rank == rank, 1, 0))
+
+# summarize timeline sequential order knowledge
+timeline.orderSum <- d.e.T %>%
+group_by(agegroup, item) %>%
+  summarize(n = n(),
+            order.m = mean(rankCor, na.rm = TRUE), # who got correct status (first answer)
+            order.sd = (sd(rankCor, na.rm = TRUE)), # standard deviation
+            order.se = order.sd/sqrt(n), # calculate standard error
+            order.lower = order.m - order.se, # calculate lower 95% CI
+            order.upper = order.m + order.se) # calculate upper 95% CI
+
+#Calculate RankDif, the difference between rank on timeline and correct rank
+d.e.T$RankDif <- abs(d.e.T$cor.rank - d.e.T$rank)
+
+# Anova of Rank error ####
+Temp.TL.aov <- aov(RankDif ~ ageyears * item + Error(subjid/(item)), d.e.T)
+summary(Temp.TL.aov) #highly sig effect of age
+
+# I think data is unbalanced so I need to use lm
+rank.lm <- lm(RankDif ~ ageyears, rank)
+summary(rank.lm)
+
+# t-tests by age of aggregate Rank error ####
+# t-tests at each age
+
+Temp.TL5 <- aggregate(RankDif ~ agegroup + subjid, subset(d.e.T), mean)
+with(subset(Temp.TL5, agegroup %in% c("3")), t.test(RankDif, mu=5))
+with(subset(Temp.TL5, agegroup %in% c("4")), t.test(RankDif, mu=5))
+with(subset(Temp.TL5, agegroup %in% c("5")), t.test(RankDif, mu=5))
+with(subset(Temp.TL5, agegroup %in% c("6")), t.test(RankDif, mu=5))
+with(subset(Temp.TL5, agegroup %in% c("7")), t.test(RankDif, mu=5))
+
+with(subset(Temp.TL5, agegroup %in% c("3","4")), t.test(RankDif~agegroup, var.equal=T))
+
+
+###left off here
+Temp.PF3 <- aggregate(BNRight ~ Age + AgeYears + SubjID, subset(Temp.PF2), mean)
+with(subset(Temp.PF3, AgeYears=="3"), t.test(x=BNRight, mu=.5))
+with(subset(Temp.PF3, AgeYears=="4"), t.test(x=BNRight, mu=.5))
+with(subset(Temp.PF3, AgeYears %in% c("6", "7")), t.test(BNRight ~ AgeYears, var.equal=T))
+with(subset(Temp.PF3, AgeYears %in% c("6", "adult")), t.test(BNRight ~ AgeYears, var.equal=T))
+with(subset(Temp.PF3, AgeYears %in% c("7", "adult")), t.test(BNRight ~ AgeYears, var.equal=T))
+with(subset(Temp.PF3, AgeYears %in% c("7", "8")), t.test(BNRight ~ AgeYears, var.equal=T))
+with(subset(Temp.PF3, AgeYears %in% c("8", "adult")), t.test(BNRight ~ AgeYears, var.equal=T))
+
+Temp.PF4 <- subset(Temp.PF3, AgeYears != "adult")
+Temp.PF4$AgeYears <- as.numeric(as.character(Temp.PF4$AgeYears))
+Temp.PF4.m1 <- lm(BNRight ~ AgeYears, Temp.PF4)
+summary(Temp.PF4.m1)
